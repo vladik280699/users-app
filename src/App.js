e35a5d027b4f7cdbf0578fee2cf14e9c9a65810a
Vlad <vladik280699@gmail.com>
2021-07-23 16:40:54 +0300

    add footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 import './App.scss'
 
 function App() {
   return <div className="App">
     <Header/>
-    Users app
+    <div className="Content">
+      Users app
+    </div>
+    <Footer/>
   </div>
 }
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Footer.scss'
+
+const Footer = () => {
+  return (
+    <footer className="Footer">
+      vlad.musiichuk28@gmail.com
+    </footer>
+  )
+}
+
+export default Footer
